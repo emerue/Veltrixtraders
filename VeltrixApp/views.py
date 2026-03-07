c4b431e78f4a184cbaeb044c231f59446f7be0d6
Nworah_Gabriel <gabrielnworah6@gmail.com>
2026-03-08 02:20:19 +0800

    Cloned the outer pages

--- VeltrixApp/views.py
+++ VeltrixApp/views.py
@@ -7,3 +7,48 @@ def home(request):
 
 def construction(request):
     return render(request, 'construction.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def software(request):
+    return render(request, 'software.html')
+
+def insight(request):
+    return render(request, 'insight.html')
+
+def copy_trading(request):
+    return render(request, 'option-copy-trading.html')
+
+def advance_trading(request):
+    return render(request, 'advance-trading.html')
+
+def live_trading(request):
+    return render(request, 'live-trading.html')
+
+def swing_trading(request):
+    return render(request, 'swing-trading.html')
+
+def feature_trading(request):
+    return render(request, 'futures.html')
+
+def options_trading(request):
+    return render(request, 'option-trading.html')
+
+def oil_and_gas(request):
+    return render(request, 'oil-and-gas.html')
+
+def terms_and_conditions(request):
+    return render(request, 'terms-and-conditions.html')
+
+def privacy_policy(request):
+    return render(request, 'privacy-policy.html')
+
+def forgot_password(request):
+    return render(request, 'forgot-password.html')
